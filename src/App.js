15d9f63d7d6567a17c3c421b32789018193a6d6b
Marin <marin.knezevic1et2@gmail.com>
2022-07-12 15:55:08 +0200

    adding links and navigation

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetups from "./pages/AllMeetups";
 import NewMeetup from "./pages/NewMeetup";
 import Favorites from "./pages/Favorites";
+import Navigation from "./components/layout/Navigation";
 //route component and we useit as a component and he defines different paths in url and which component should be loaded for the different paths
 //with switch component we are telling react that only one router should be open
 function App() {
   return (
     <div>
+      <Navigation />
       <Switch>
         <Route path="/" exact>
           <AllMeetups />
--- src/components/layout/Navigation.js
+++ src/components/layout/Navigation.js
@@ -1,6 +1,7 @@
 //we are doing this in different folder because it will not be rendered as routes but instead it will embedded and visible constantly,e its a navigation bar!!
 import { Link } from "react-router-dom";
-//link is not sending new request but instead just parse the url we want to go to, change it in the browser url bar and then loads the appropriate component on the screen using js and react
+import React from "react";
+//link is not sending new request but instead just parse the url we want to go to, change it in the browser url bar and then loads the appropriate component on the screen using js and react, so we stay on the loaded page without sending extra request
 function Navigation() {
   return (
     <header>
@@ -8,7 +9,13 @@ function Navigation() {
       <nav>
         <ul>
           <li>
-            <Link> Something </Link>
+            <Link to="/"> All meetups </Link>
+          </li>
+          <li>
+            <Link to="/new-meetup"> Add Meetup </Link>
+          </li>
+          <li>
+            <Link to="/favorites"> My favorites </Link>
           </li>
         </ul>
       </nav>
