a95bb6815a51a7258a39cb91223beb05bfaafa0b
Marin <marin.knezevic1et2@gmail.com>
2022-07-12 16:05:56 +0200

    css moduels for navigation

--- src/components/layout/Navigation.js
+++ src/components/layout/Navigation.js
@@ -1,11 +1,14 @@
 //we are doing this in different folder because it will not be rendered as routes but instead it will embedded and visible constantly,e its a navigation bar!!
 import { Link } from "react-router-dom";
 import React from "react";
+import classes from "./Navigation.module.css";
 //link is not sending new request but instead just parse the url we want to go to, change it in the browser url bar and then loads the appropriate component on the screen using js and react, so we stay on the loaded page without sending extra request
 function Navigation() {
   return (
-    <header>
-      <div>Meetups</div>
+    <header className={classes.header}>
+      <div>
+        <h1>Meetups</h1>
+      </div>
       <nav>
         <ul>
           <li>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import App from "./App.js";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
