502ec83e8ad57628b4a3b5efe0af0e5caa0ea8ed
Marin <marin.knezevic1et2@gmail.com>
2022-07-13 14:58:20 +0200

    props.children nice practice

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetups from "./pages/AllMeetups";
 import NewMeetup from "./pages/NewMeetup";
 import Favorites from "./pages/Favorites";
-import Navigation from "./components/layout/Navigation";
+import Layout from "./components/layout/Layout";
 //route component and we useit as a component and he defines different paths in url and which component should be loaded for the different paths
 //with switch component we are telling react that only one router should be open
 function App() {
   return (
-    <div>
-      <Navigation />
+    <Layout>
       <Switch>
         <Route path="/" exact>
           <AllMeetups />
@@ -21,7 +20,7 @@ function App() {
           <Favorites />
         </Route>
       </Switch>
-    </div>
+    </Layout>
   );
 }
 
