e1f9eb678d0cf69834c16bea15f6bd461ea60d4b
Marin <marin.knezevic1et2@gmail.com>
2022-07-13 15:04:26 +0200

    adding a form, placing data onto the server

---
+++ src/components/meetups/NewMeetupForm.js
@@ -0,0 +1,6 @@
+//here we are working on placing the data onto the server
+import classes from "./NewMeetupForm.module.css";
+
+function NewMeetupForm() {}
+
+export default NewMeetupForm;
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,7 +1,13 @@
 import React from "react";
-
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+//storing the data on the server a little bit of backend
 function NewMeetup() {
-  return <div>New Meetup</div>;
+  return (
+    <section>
+      <h1>New meetup</h1>
+      <NewMeetup />
+    </section>
+  );
 }
 
 export default NewMeetup;
