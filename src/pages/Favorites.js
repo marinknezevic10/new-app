ef996e3dfcc54f921346a33592af60d14e3d5afd
Marin <marin.knezevic1et2@gmail.com>
2022-07-14 16:48:46 +0200

    adding favorites finishing

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { useContext } from "react";
+import FavoritesContext from "../store/favorites-context";
+import MeetupList from "../components/meetups/MeetupList";
+//outputting favorites on the page
+//we are using usecontext because we want to output all the favorites from our context
 
 function Favorites() {
-  return <div>Favorite meetup</div>;
-}
+  const favoritesCtx = useContext(FavoritesContext);
 
+  //helper variable content to check if we have favorites
+  let content;
+  //if we have no favorites output "You have no favorites" inače output the favorites
+  if (favoritesCtx.totalFavorites === 0) {
+    content = <p>You have no favorites!</p>;
+  } else {
+    content = <MeetupList meetups={favoritesCtx.favorites} />;
+  }
+
+  return (
+    <section>
+      <h1>My favorite meetups</h1>
+      {content}
+    </section>
+  );
+}
 export default Favorites;
