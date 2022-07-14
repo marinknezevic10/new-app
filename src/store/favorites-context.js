bdf63bf014b828feca838904651c1c2eeba621d7
Marin <marin.knezevic1et2@gmail.com>
2022-07-14 14:33:08 +0200

    react context, dont really get it

---
+++ src/store/favorites-context.js
@@ -0,0 +1,26 @@
+import { createContext, useState } from "react";
+//page for adding to favorites
+//storing it in a constant
+const FavoritesContext = createContext({
+  favorites: [],
+  totalFavorites: 0,
+});
+
+//this component will have a job of providing this context to all the components that are interested in listenin to the provided values
+//this function also responsible for updating the context values
+//when we manage state in here the component will execute again, if we change our context value and we pass it to provider all components that are listening to our values will be updated
+function FavoritesContextProvider(props) {
+  const [userFavorites, setUserFavorites] = useState([]);
+
+  const context = {
+    favorites: userFavorites,
+    totalFavorites: userFavorites.length,
+  };
+
+  return (
+    //value prop helps us to update new values
+    <FavoritesContext.Provider value={context}>
+      {props.children}
+    </FavoritesContext.Provider>
+  );
+}
